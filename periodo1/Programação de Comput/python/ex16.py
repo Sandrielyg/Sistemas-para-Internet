38224d4f788a34607154da33827f29c8da3a0602
Sandriely Gonçalves <130182483+Sandrielyg@users.noreply.github.com>
2023-07-19 20:41:42 -0300

    Update ex16.py

--- periodo1/Programação de Comput/python/ex16.py
+++ periodo1/Programação de Comput/python/ex16.py
@@ -2,3 +2,10 @@ idades = [int(input("Digite a primeira idade: ")),
           int(input("Digite a segunda idade: ")),
           int(input("Digite a terceira idade: "))]
 print (idades)
+
+..
+
+idades = [int(input("Digite a primeira idade: ")),
+          int(input("Digite a segunda idade: ")),
+          int(input("Digite a terceira idade: "))]
+print (idades [1])
