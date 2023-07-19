aba4d49cbf1471394e0a90e7fe30807593e555fa
Sandriely Gonçalves <130182483+Sandrielyg@users.noreply.github.com>
2023-07-19 19:56:58 -0300

    Create exemplo11.py

---
+++ periodo1/Programação de Comput/python/exemplo11.py
@@ -0,0 +1,9 @@
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+num3 = int(input("Digite o terceiro número: "))
+maior = num1  
+if num2 > maior:
+    maior = num
+elif num3 > maior:
+    maior = num3
+print("O maior número é:", maior)
