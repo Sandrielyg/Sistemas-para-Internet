5a807076fe9a42a0872d706c343e8c8c52e8dac9
Sandriely Gonçalves <130182483+Sandrielyg@users.noreply.github.com>
2023-07-19 20:03:32 -0300

    Create ex04.py

---
+++ periodo1/Programação de Comput/python/ex04.py
@@ -0,0 +1,6 @@
+anodenascimento = int (input("Informe o ano em que você nasceu. \n"))
+anoatual = int(input("Agora informe em que ano você está. \n"))
+idade = anoatual - anodenascimento
+print ("Você tem" , idade, "anos! \n")
+idademaistarde = 2025 - anodenascimento
+print ("E em 2025 você terá", idademaistarde, "anos!")
